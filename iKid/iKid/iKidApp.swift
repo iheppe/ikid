5907d541c4f255620a5ee9efb6eb7e780f197153
iheppe <iheppe@uw.edu>
2021-05-04 02:07:02 -0700

    add new files

---
+++ iKid/iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by Isabella Heppe on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
